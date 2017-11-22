fc314bef01cda8c66201590e952df408cf18d868
Gonsolin <val.gonsolin@gmail.com>
2017-11-22 23:49:57 +0000

    comentaire

--- a_propos.php
+++ a_propos.php
@@ -7,8 +7,9 @@ drawheader();
 <p style="text-align: center; padding-top: 20px;">
 Ce site à été développé par Quentin ROCHE lors d'un stage de 7 semaines sur le site de Faurecia Beaulieu.<br>
 Il a été ensuite amélioré par Junior CentraleSupélec lors d'une mission en 2017.<br>
-    
+
 Pour tout bug ou modification merci de contacter <a href="http://juniorcs.fr">Junior CentraleSupélec</a>.
 </p>
 <?php
 drawFooter();
+//salut abega
