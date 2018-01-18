258b17cbdcc3b2feaf4a0182a9169c1b2fc0fd2c
abegathomas <abegathomas282@gmail.com>
2018-01-18 18:26:57 +0100

    idee

--- RH/idees_ameliorations/idees_tot.php
+++ RH/idees_ameliorations/idees_tot.php
@@ -6,7 +6,7 @@ drawHeader('RH');
 drawMenu('tot_idees');
 
 
-$recherche = "";
+$recherche = -1;
 $debut=0;
 
 
@@ -38,7 +38,15 @@ else
   <form class="form-inline">
   <div class="form-group">
     <label for="recherche">Recherche :</label>
-    <input type="text" class="form-control" name = "recherche" id="recherche" placeholder="Nom, Prénom" value="<?php echo $recherche;?>">
+    <select class="form-control" name="recherche" >
+      <?php
+      $profil = $bdd -> query('SELECT * FROM profil');
+      while($personne = $profil -> fetch()){ ?>
+        <option value="<?php echo $personne['id']; ?>" ><?php echo $personne['nom']." ".$personne['prenom']; ?></option>
+    <?php  } ?>
+    </select>
+
+
   </div>
   <button type="submit" class="btn btn-default">Rechercher</button>
   <a href="ajout.php" class="btn btn-default pull-right">Espace administration</a>
@@ -62,9 +70,11 @@ else
 
 <?php
 
-
-$Query = $bdd->prepare('SELECT nom,prenom,type,date_rea,vote,idees_ameliorations.id AS id1 FROM idees_ameliorations LEFT JOIN profil  ON idees_ameliorations.emmetteur = profil.id  WHERE (nom LIKE ? or prenom LIKE ?) and supprime = 0 and idees_ameliorations.id >= ?  ORDER BY date_rea LIMIT 40  ') ;
-$Query->execute(array('%'.$recherche.'%', '%'.$recherche.'%',$debut));
+if($recherche>=0){
+$Query = $bdd->prepare('SELECT nom,prenom,type,date_rea,vote,idees_ameliorations.id AS id1 FROM idees_ameliorations LEFT JOIN profil  ON idees_ameliorations.emmetteur = profil.id  WHERE profil.id= ? and supprime = 0  ORDER BY date_rea LIMIT 40 OFFSET ? ') ;
+$Query->execute(array($recherche,$debut));}
+else{$Query = $bdd->prepare('SELECT nom,prenom,type,date_rea,vote,idees_ameliorations.id AS id1 FROM idees_ameliorations LEFT JOIN profil  ON idees_ameliorations.emmetteur = profil.id  WHERE supprime = 0  ORDER BY date_rea LIMIT 40 OFFSET ? ') ;
+$Query->execute(array($debut));}}
 
 while ($Data = $Query->fetch()) {
     ?>
@@ -82,8 +92,9 @@ while ($Data = $Query->fetch()) {
 
         if($Qy->fetch()){echo "<span style='font-size: 200%;'>&check;</span>";}else{echo "<span style='font-size: 150%;'>&#10008;</span>";} ?>
       </td>
-
-        <td class="clickable" title="Cliquez pour voter/voir le detail " onclick="window.location='details.php?idee=<?php echo $Data['id1'] ;?>'">Voter/Voir details</td>
+      <?php
+        if($Qy->fetch()){echo "<td class="clickable" title="Cliquez pour voter/voir le detail " onclick="window.location='details.php?idee=<?php echo $Data['id1'] ;?>'">Voter/Voir details</td>";}
+        else{echo "<td class="clickable" title="Cliquez pour retirer vote/voir le detail " onclick="window.location='details.php?idee2=<?php echo $Data['id1'] ;?>'">Retirer vote/Voir details</td>";}?>
 
 
     </tr>
@@ -101,14 +112,28 @@ if($debut > 39){
   <a href="index.php?recherche=<?php echo $recherche;?>&amp;nb=<?php echo $debut-40;?>" class="btn btn-default">Elements précédents</a>
 <?php
 }
+
+if($recherche>=0){
 $test = $bdd->prepare('SELECT * FROM profil LEFT JOIN idees_ameliorations
     ON idees_ameliorations.emmetteur = profil.id
-    WHERE (nom LIKE ? or prenom LIKE ?) and supprime = 0 and idees_ameliorations.id >= ? LIMIT 40   ');
-$test->execute(array('%'.$recherche.'%', '%'.$recherche.'%', ($debut+40)));
+    WHERE profil.id= ? and supprime = 0 and idees_ameliorations.id >= ? LIMIT 40   ');
+$test->execute(array($recherche, ($debut+40)));
 if($test -> fetch()){ ?>
   <a href="index.php?recherche=<?php echo $recherche;?>&amp;nb=<?php echo $debut+40;?>" class="btn btn-default">Elements suivants</a>
+
 <?php
 }
+}else{$test = $bdd->prepare('SELECT * FROM profil LEFT JOIN idees_ameliorations
+    ON idees_ameliorations.emmetteur = profil.id
+    WHERE supprime = 0 and idees_ameliorations.id >= ? LIMIT 40   ');
+$test->execute(array( ($debut+40)));
+if($test -> fetch()){ ?>
+  <a href="index.php?recherche=<?php echo $recherche;?>&amp;nb=<?php echo $debut+40;?>" class="btn btn-default">Elements suivants</a>
+
+<?php
+}
+}
+
 }
 drawFooter();
  ?>
