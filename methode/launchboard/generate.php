caaef2327c0697ea70be849177356c884612b0fe
Gonsolin <33912492+valgonsolin@users.noreply.github.com>
2018-02-02 01:11:11 +0100

    advance

--- methode/launchboard/generate.php
+++ methode/launchboard/generate.php
@@ -2,21 +2,105 @@
 ob_start();
 include_once "../../needed.php";
 require '../../php/spreadsheet/vendor/autoload.php';
-$file = 'base.xlsx';
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Cell;
+$input = 'base.xlsx';
+$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($input);
 
-$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx");
-$spreadsheet = $reader->load($file);
+$sheet = $spreadsheet->getActiveSheet();
+/* $xls_data contains this array:
+[1=>['A'=>'Domain', 'B'=>'Category', 'C'=>'Nr. Pages'], 2=>['A'=>'CoursesWeb.net', 'B'=>'Web Development', 'C'=>4000], 3=>['A'=>'MarPlo.net', 'B'=>'Courses & Games', 'C'=>15000]]
+*/
+function copyRange( Worksheet $sheet, $srcRange, $dstCell) {
+    // Validate source range. Examples: A2:A3, A2:AB2, A27:B100
+    if( !preg_match('/^([A-Z]+)(\d+):([A-Z]+)(\d+)$/', $srcRange, $srcRangeMatch) ) {
+        // Wrong source range
+        return;
+    }
+    // Validate destination cell. Examples: A2, AB3, A27
+    if( !preg_match('/^([A-Z]+)(\d+)$/', $dstCell, $destCellMatch) ) {
+        // Wrong destination cell
+        return;
+    }
 
+    $srcColumnStart = $srcRangeMatch[1];
+    $srcRowStart = $srcRangeMatch[2];
+    $srcColumnEnd = $srcRangeMatch[3];
+    $srcRowEnd = $srcRangeMatch[4];
 
-$spreadsheet->setActiveSheetIndex(0)
- ->setCellValue('AI7', '15%');
-$spreadsheet->setActiveSheetIndex(0)
- ->setCellValue('B30', '1')
- ->setCellValue('S30', '18/09/1997')
- ->setCellValue('C30', 'titre');
+    $destColumnStart = $destCellMatch[1];
+    $destRowStart = $destCellMatch[2];
 
-$writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, "Xlsx");
-$writer->save('temp.xlsx');
+    // For looping purposes we need to convert the indexes instead
+    // Note: We need to subtract 1 since column are 0-based and not 1-based like this method acts.
+
+    $srcColumnStart = Cell::columnIndexFromString($srcColumnStart) - 1;
+    $srcColumnEnd = Cell::columnIndexFromString($srcColumnEnd) - 1;
+    $destColumnStart = Cell::columnIndexFromString($destColumnStart) - 1;
+
+    $rowCount = 0;
+    for ($row = $srcRowStart; $row <= $srcRowEnd; $row++) {
+        $colCount = 0;
+        for ($col = $srcColumnStart; $col <= $srcColumnEnd; $col++) {
+            $cell = $sheet->getCellByColumnAndRow($col, $row);
+            $style = $sheet->getStyleByColumnAndRow($col, $row);
+            $dstCell = Cell::stringFromColumnIndex($destColumnStart + $colCount) . (string)($destRowStart + $rowCount);
+            $sheet->setCellValue($dstCell, $cell->getValue());
+            $sheet->duplicateStyle($style, $dstCell);
+
+            // Set width of column, but only once per row
+            if ($rowCount === 0) {
+                $w = $sheet->getColumnDimensionByColumn($col)->getWidth();
+                $sheet->getColumnDimensionByColumn ($destColumnStart + $colCount)->setAutoSize(false);
+                $sheet->getColumnDimensionByColumn ($destColumnStart + $colCount)->setWidth($w);
+            }
+
+            $colCount++;
+        }
+
+        $h = $sheet->getRowDimension($row)->getRowHeight();
+        $sheet->getRowDimension($destRowStart + $rowCount)->setRowHeight($h);
+
+        $rowCount++;
+    }
+
+    foreach ($sheet->getMergeCells() as $mergeCell) {
+        $mc = explode(":", $mergeCell);
+        $mergeColSrcStart = Cell::columnIndexFromString(preg_replace("/[0-9]*/", "", $mc[0])) - 1;
+        $mergeColSrcEnd = Cell::columnIndexFromString(preg_replace("/[0-9]*/", "", $mc[1])) - 1;
+        $mergeRowSrcStart = ((int)preg_replace("/[A-Z]*/", "", $mc[0]));
+        $mergeRowSrcEnd = ((int)preg_replace("/[A-Z]*/", "", $mc[1]));
+
+        $relativeColStart = $mergeColSrcStart - $srcColumnStart;
+        $relativeColEnd = $mergeColSrcEnd - $srcColumnStart;
+        $relativeRowStart = $mergeRowSrcStart - $srcRowStart;
+        $relativeRowEnd = $mergeRowSrcEnd - $srcRowStart;
+
+        if (0 <= $mergeRowSrcStart && $mergeRowSrcStart >= $srcRowStart && $mergeRowSrcEnd <= $srcRowEnd) {
+            $targetColStart = Cell::stringFromColumnIndex($destColumnStart + $relativeColStart);
+            $targetColEnd = Cell::stringFromColumnIndex($destColumnStart + $relativeColEnd);
+            $targetRowStart = $destRowStart + $relativeRowStart;
+            $targetRowEnd = $destRowStart + $relativeRowEnd;
+
+            $merge = (string)$targetColStart . (string)($targetRowStart) . ":" . (string)$targetColEnd . (string)($targetRowEnd);
+            //Merge target cells
+            $sheet->mergeCells($merge);
+        }
+    }
+}
+
+// $file->setActiveSheetIndex(0)
+//  ->setCellValue('A1', 'Domain')
+//  ->setCellValue('B1', 'Category')
+//  ->setCellValue('C1', 'Nr. Pages');
+copyrange($sheet,'B114:AO120','B22');
+
+$writer = new Xlsx($spreadsheet);
+$output ='temp.xlsx';
+$writer->save($output);
+
 header('Content-Description: File Transfer');
 header('Content-Type: application/ms-excel');
 header("Content-Disposition: attachment; filename=launchroom.xlsx");
@@ -24,3 +108,18 @@ ob_clean();
 flush();
 readfile('temp.xlsx');
 exit;
+
+
+//set style for A1,B1,C1 cells
+// $cell_st =[
+//  'font' =>['bold' => true],
+//  'alignment' =>['horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER],
+//  'borders'=>['bottom' =>['style'=> \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM]]
+// ];
+// $spreadsheet->getActiveSheet()->getStyle('A1:C1')->applyFromArray($cell_st);
+//
+// //set columns width
+// $spreadsheet->getActiveSheet()->getColumnDimension('A')->setWidth(16);
+// $spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(18);
+//
+// $spreadsheet->getActiveSheet()->setTitle('Simple'); //set a title for Worksheet
