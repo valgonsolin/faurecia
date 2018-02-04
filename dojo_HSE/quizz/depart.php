4477a7b7e49591eae8aa56a78d253e70c3b581aa
abegathomas <abegathomas282@gmail.com>
2018-02-04 16:41:16 +0100

    le meilleur c'est la leuleu

--- dojo_HSE/quizz/depart.php
+++ dojo_HSE/quizz/depart.php
@@ -8,7 +8,7 @@ include_once "../needed.php";
 
 
 $Query = $bdd->prepare('SELECT * FROM profil WHERE id = ?');
-$Query->execute(array($_GET["id_personne"]));
+$Query->execute(array($_SESSION['id']));
 $mo = $Query->fetch()['mo'];
 if ($mo == "MOD"){
     $type = 0;
@@ -17,13 +17,13 @@ if ($mo == "MOD"){
 }
 
 $Query = $bdd->prepare('SELECT * FROM qualite_hse_session WHERE personne = ? and fin is NULL');
-$Query->execute(array($_GET["id_personne"]));
+$Query->execute(array($_SESSION['id']));
 if ($Data = $Query->fetch()) {
     ob_end_clean();
     header('Location: '.$url."/dojo_HSE/quizz/question.php?id=".$Data['id']);
 }else{
     $Query = $bdd->prepare('INSERT INTO qualite_hse_session SET personne = ?, qualite_hse_session.type = ?');
-    $Query->execute(array($_GET["id_personne"],$type));
+    $Query->execute(array($_SESSION['id'],$type));
     ob_end_clean();
     header('Location: '.$url."/dojo_HSE/quizz/question.php?id=".$bdd->lastInsertId());
 }
