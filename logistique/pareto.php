1262a5759dffde720837179b2c5b4aa53c423818
abegathomas <abegathomas282@gmail.com>
2018-04-05 11:36:23 +0200

    dvdq

--- logistique/pareto.php
+++ logistique/pareto.php
@@ -1,13 +1,81 @@
 <?php
 ob_start();
 include_once "../needed.php";
-
+$datetime = date("Y-m-d");
+$intervalle=30;
 drawheader();
-
+if(isset($_POST['intervalle'])){
+	$intervalle=$_POST['duree'];
+  }
 ?>
 <h2>Pareto alerte</h2>
 
-//SELECT logistique_pieces.fournisseur as f,COUNT(*) as count FROM logistique_alerte JOIN logistique_pieces ON logistique_alerte.piece = logistique_pieces.id  GROUP BY fournisseur ORDER BY count DESC
+<br>
+<form method="post" style="margin-top:20px;" enctype="multipart/form-data">
+
+  <div class="form-group">
+  <label>Choisissez un intervalle de temps </label>
+  <select name="duree" class="form-control">
+    <option value="30" selected="selected">30 jours</option>
+    <option value="90">90 jours</option>
+
+</select>
+</div>
+
+<button type="submit" name="intervalle" class="btn btn-default" >Appliquer</button>
+
+</form>
+<div id="container" style="width: 75%; height: 20%;">
+		<canvas id="canvas"></canvas>
+	</div>
+
+<?php 
+$query=$bdd->prepare('SELECT logistique_pieces.fournisseur as f,COUNT(*) as count FROM logistique_alerte JOIN logistique_pieces ON logistique_alerte.piece = logistique_pieces.id WHERE DATEDIFF(?,logistique_alerte.date )<= ? GROUP BY fournisseur ORDER BY count DESC');
+$query->execute(array($datetime, $intervalle));
+$f=$query->fetchAll();
+?>
+
+
+
+	  <script src="../js/loader.js"></script>
+  	  <script src="../js/moment.min.js"></script>
+      <script src="../js/Chart.js"></script>
+      <canvas id="myChart" ></canvas>
+<script>
+
+
+var fournisseur =new Array();
+var nbfournisseur =new Array();
+<?php
+foreach ($f as $fou){
+  ?>fournisseur.push("<?php echo $fou['f']; ?>");
+    nbfournisseur.push("<?php echo $fou['count']; ?>");
+
+<?php } ?>;
+
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: fournisseur,
+        datasets: [{
+            label: 'Pareto par fournisseur',
+            data: nbfournisseur,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
 
 <?php
 drawFooter();
